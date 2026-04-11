c40ea5b97da5d07e9559f711dbb66a711c9dbfab
torodriguezt <torodriguezt@unal.edu.co>
2026-04-10 23:57:16 -0500

    [prototype] Add imports and define __all__ for main module exports

--- uncertainty_explainer/__init__.py
+++ uncertainty_explainer/__init__.py
@@ -1,1 +1,10 @@
 from uncertainty_explainer import config  # noqa: F401
+from uncertainty_explainer.conformal.predictor import ConformalPredictor
+from uncertainty_explainer.explainability.explainer import UncertaintyShapExplainer
+from uncertainty_explainer.uq_explainer import UncertaintyExplanationPipeline
+
+__all__ = [
+    "ConformalPredictor",
+    "UncertaintyShapExplainer",
+    "UncertaintyExplanationPipeline",
+]
