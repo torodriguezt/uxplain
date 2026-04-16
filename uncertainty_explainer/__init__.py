9fb5f5e4f0bea1ecd48f678d8e3a379c88c5c39e
torodriguezt <torodriguezt@unal.edu.co>
2026-04-15 22:24:08 -0500

    [prototype] Add PDP explanation classes to the module exports

--- uncertainty_explainer/__init__.py
+++ uncertainty_explainer/__init__.py
@@ -4,6 +4,10 @@ from uncertainty_explainer.explainability.lime_explainer import (
     LIMEExplanation,
     LimeUncertaintyExplainer,
 )
+from uncertainty_explainer.explainability.pdp_explainer import (
+    PDPExplanation,
+    PDPUncertaintyExplainer,
+)
 from uncertainty_explainer.explainability.shap_explainer import ShapUncertaintyExplainer
 from uncertainty_explainer.protocols import (
     ConformalPredictorProtocol,
@@ -21,6 +25,8 @@ __all__ = [
     "ExplanationResult",
     "LIMEExplanation",
     "LimeUncertaintyExplainer",
+    "PDPExplanation",
+    "PDPUncertaintyExplainer",
     "ShapUncertaintyExplainer",
     "UncertaintyExplainerProtocol",
     "UncertaintyExplanationPipeline",
--- uncertainty_explainer/explainability/__init__.py
+++ uncertainty_explainer/explainability/__init__.py
@@ -2,6 +2,10 @@ from uncertainty_explainer.explainability.lime_explainer import (
     LIMEExplanation,
     LimeUncertaintyExplainer,
 )
+from uncertainty_explainer.explainability.pdp_explainer import (
+    PDPExplanation,
+    PDPUncertaintyExplainer,
+)
 from uncertainty_explainer.explainability.shap_explainer import (
     ShapUncertaintyExplainer,
 )
@@ -9,5 +13,7 @@ from uncertainty_explainer.explainability.shap_explainer import (
 __all__ = [
     "LIMEExplanation",
     "LimeUncertaintyExplainer",
+    "PDPExplanation",
+    "PDPUncertaintyExplainer",
     "ShapUncertaintyExplainer",
 ]
