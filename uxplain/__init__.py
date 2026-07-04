9f2a770eca7c7a2d4725f9af753475a824a2912a
torodriguezt <torodriguezt@unal.edu.co>
2026-07-04 13:34:09 -0500

    chore: bump version to 0.2.3

--- uxplain/__init__.py
+++ uxplain/__init__.py
@@ -1,6 +1,6 @@
 """uxplain: explainability for conformal prediction uncertainty."""
 
-__version__ = "0.2.2"
+__version__ = "0.2.3"
 
 from uxplain.conformal.cqr_predictor import CQRConformalPredictor
 from uxplain.conformal.crepes_classifier import (
